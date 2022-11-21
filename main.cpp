ad636e71bf032cf3eeee598705dfb1f03d772752
J0k3s0nY0u <109585635+J0k3s0nY0u@users.noreply.github.com>
2022-11-21 20:25:25 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+ #include <iostream>
+ #include "Functions.h"
+ #include "Game.h"
+
+using namespace std;
+
+int main() {
+	srand(time(NULL));
+	
+	Game game;
+	while (game.getPlaying())
+	{
+		game.mainMenu();
+	}
+	
+	
+	return 0;
+}
